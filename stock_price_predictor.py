38c3780795583b90354c974613bb473e38f66afc
Hernan Razo <hernanrazo@gmail.com>
2018-06-06 15:42:14 -0500

    incorporated pandas

--- stock_price_predictor.py
+++ stock_price_predictor.py
@@ -1,40 +1,45 @@
-import csv
-import numpy as np
-from sklearn.svm import SVR
-import matplotlib.pyplot as plt 
 
-plt.switch_backend('macosx')
+import matplotlib.pyplot as plt 
+from matplotlib import style
+from sklearn.svm import SVR
+import pandas as pd
+import numpy as np
+import csv
+
+# style.use('ggplot')
 
 dates = []
-prices = []
+closing_price = []
+ 
+df = pd.read_csv('/Users/hernanrazo/pythonProjects/stock_price_predictor/AMZN.csv', engine = 'python')
 
-#TODO: incorporate pandas instead.
-def get_data(filename):
-	with open(filename, 'r') as csvfile:
-		csvFileReader = csv.reader(csvfile)
+for row in df:
 
-		next(csvFileReader)
-		for row in csvFileReader:
+	dates = df.Date.values
+	closing_price.append(df.Close)
 
-			dates.append(int(row[0].split('-')[0]))
-			prices.append(float(row[1]))
-	return
 
-def predict_price(dates, prices, x):
-	dates = np.reshape(dates,(len(dates), 1))
+for i in dates:
+	dates.append(int(row[0]).split('/')[0])
+
+
+
+
+def predict_price(dates, closing_price, x):
 
 	svr_lin = SVR(kernel = 'linear', C = 1e3)
 	svr_poly = SVR(kernel = 'poly', C = 1e3, degree = 2)
 	svr_rbf = SVR(kernel = 'rbf', C = 1e3, gamma = 0.1)
 	
-	svr_rbf.fit(dates, prices)
-	svr_lin.fit(dates, prices)
-	svr_poly.fit(dates, prices)
+	svr_lin.fit(dates, closing_price)
+	svr_poly.fit(dates, closing_price)
+	svr_rbf.fit(dates, closing_price)
 
 	plt.scatter(dates, prices, color= 'black', label = 'Data')
-	plt.plot(dates, svr_rbf.predict(dates), color = 'red', label = 'RBF model')
 	plt.plot(dates, svr_lin.predict(dates), color = 'green', label = 'Linear model')
 	plt.plot(dates, svr_poly.predict(dates), color = 'blue', label = 'Polynomial model')
+	plt.plot(dates, svr_rbf.predict(dates), color = 'red', label = 'RBF model')
+
 	plt.xlabel('Date')
 	plt.ylabel('Price')
 	plt.title('AMZN Stock Prediction')
@@ -44,7 +49,15 @@ def predict_price(dates, prices, x):
 
 	return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
 
-get_data('AMZN.csv')
-predicted_price = predict_price(dates, prices, 29)
+predicted_price = predict_price(dates, closing_price, 29)
 print(predicted_price)
+
+
+# dates = df.Date.values
+# closing_price = df.Close.values
+# print(dates)
+# print(closing_price)
+
+
+
 
