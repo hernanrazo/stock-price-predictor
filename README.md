f078a851b27dbb2e0934c53703314666ddebc788
Hernan Razo <hernanrazo@gmail.com>
2018-06-11 17:26:25 -0500

    Finished incorporating pandas. Added to README.md

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 Stock Price Predictor
 ===
 
-An attempt at using sckit learn to predict stock prices. This example uses data from Amazon's stock prices from 5/15/97 to 6/1/18. currently a work in progress. not sure if anything accurate or useful will come out of this. 
+An attempt at using scikit learn to predict stock prices. This example uses data from Amazon's stock prices from 5/15/97 to 6/1/18. To make a prediction, This program works by using support vector regression to predict the next value in a dataset using a margin of error.   
+
+Disclaimer:
+---
+This script was mainly used to showcase support vector regression and the usefulness of the pandas library to read data from a .csv file. Predicting stocks will probably need more data and other inputs of information like sentiment analysis. Do not bid your money using this program.
 
 
-Sources:
+Sources and Helpful links:
 ---
 https://finance.yahoo.com/quote/AMZN/history?period1=863672400&period2=1527915600&interval=1d&filter=history&frequency=1d  
-https://www.youtube.com/watch?v=SSu00IRRraY
+http://www.saedsayad.com/support_vector_machine_reg.htm  
+https://www.youtube.com/watch?v=SSu00IRRraY  
+https://www.youtube.com/watch?v=yCgJGsg0Xa4
--- stock_price_predictor.py
+++ stock_price_predictor.py
@@ -7,19 +7,31 @@ import numpy as np
 import csv
 
 style.use('ggplot')
- 
+
+#read csv file using pandas 
 df = pd.read_csv('/Users/hernanrazo/pythonProjects/stock_price_predictor/AMZN.csv', engine = 'python')
-dates = df.index.values
+
+#convert date column to type datetime64 
+df['Date'] = pd.to_datetime(df.Date)
+
+#return only day numbers
+dates = df.Date.dt.day
+
+#return only closing price values
 closing_price = df['Close'].values
 
-closing_price.reshape(1, -1)
 
 def predict_price(dates, closing_price, x):
+	
+	#reshape dates array 
+	dates = np.reshape(dates,(len(dates), 1))
 
+	#create models
 	svr_lin = SVR(kernel = 'linear', C = 1e3)
 	svr_poly = SVR(kernel = 'poly', C = 1e3, degree = 2)
 	svr_rbf = SVR(kernel = 'rbf', C = 1e3, gamma = 0.1)
 	
+	#train models
 	svr_lin.fit(dates, closing_price)
 	svr_poly.fit(dates, closing_price)
 	svr_rbf.fit(dates, closing_price)
@@ -29,12 +41,12 @@ def predict_price(dates, closing_price, x):
 	plt.plot(dates, svr_poly.predict(dates), color = 'blue', label = 'Polynomial model')
 	plt.plot(dates, svr_rbf.predict(dates), color = 'red', label = 'RBF model')
 
+	#format labels and overall appearance of graph
 	plt.xlabel('Date')
 	plt.ylabel('Price')
 	plt.title('AMZN Stock Prediction')
 	plt.legend()
 	plt.show()
-	plt.savefig('/Users/hernanrazo/pythonProjects/stock_price_predictor/graph.png')
 
 	return svr_rbf.predict(x)[0], svr_lin.predict(x)[0], svr_poly.predict(x)[0]
 
